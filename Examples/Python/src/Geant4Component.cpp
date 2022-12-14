405e15c9ab6e0dfc1326ce4761406ac5aed9079f
Agustin Romero <89935000+aer7@users.noreply.github.com>
2022-12-14 08:08:24 -0800

    Revert "Geant4Component includes Geant4Output2ROOT directly"
    This reverts commit 59c678038260c5e0d16ea01372f3e63182e33dec.

--- Examples/Python/src/Geant4Component.cpp
+++ Examples/Python/src/Geant4Component.cpp
@@ -19,7 +19,6 @@
 #include "ActsExamples/Geant4/SensitiveSurfaceMapper.hpp"
 #include "ActsExamples/Geant4/SimParticleTranslation.hpp"
 #include "ActsExamples/DDG4/DDG4RootRunAction.hpp"
-#include <DDG4/Geant4Output2ROOT.h>
 
 #include <memory>
 
