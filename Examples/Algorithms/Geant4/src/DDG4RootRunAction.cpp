e31e0a349b18c22e0d2a67d9e7a34ab51c04e99a
Agustin Romero <89935000+aer7@users.noreply.github.com>
2022-12-14 08:08:25 -0800

    Revert "Compiles, includes Geant4Output2ROOT"
    This reverts commit 449f7b665b274682b9df19f7c8e19cb47b88ae35.

--- Examples/Algorithms/Geant4/src/DDG4RootRunAction.cpp
+++ Examples/Algorithms/Geant4/src/DDG4RootRunAction.cpp
@@ -6,11 +6,36 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "ActsExamples/DDG4/DDG4RootRunAction.hpp"
+#include "ActsExamples/DDG4/RootRunAction.hpp"
 #include <stdexcept>
 #include <G4Run.hh>
 #include <DDG4/Geant4Output2ROOT.h>
+#include "EventAction.hpp"
 
 namespace ActsExamples::DDG4RootRunAction {
+
+RunAction* RunAction::s_instance = nullptr;
+  
+RunAction* RunAction::instance() {
+  return s_instance;
+}
+
+RunAction::RunAction() : G4UserRunAction() {
+  if (s_instance != nullptr) {
+    throw std::logic_error("Attempted to duplicate the RunAction singleton");
+  } else {
+    s_instance = this;
+  }
+}
+
+RunAction::~RunAction() {
+  s_instance = nullptr;
+}
+
+void RunAction::BeginOfRunAction(const G4Run* /*unused*/) {
+  EventAction::instance()->clear();
+}
+
+void RunAction::EndOfRunAction(const G4Run* /*unused*/) {}
 
 }
